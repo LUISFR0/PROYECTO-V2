f084db44b724747cd84cbc82f5261f8cb9cb8c3e
GabuasLuis <luisjeremy76@gmail.com>
2026-01-15 21:55:54 -0600

    crear proveedor

--- provedores/index.php
+++ provedores/index.php
@@ -395,7 +395,7 @@ if (isset($_SESSION['mensaje'])) {
         <div class="modal-dialog modal-lg">
           <div class="modal-content">
             <div class="modal-header" style="background-color: #1149e2ff; color: white;">
-              <h4 class="modal-title">Create Categorie</h4>
+              <h4 class="modal-title">Create Proveedor</h4>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
