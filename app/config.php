887705540a79f3ac31247cec6daaf327b3f7a259
GabuasLuis <luis209207@gmail.com>
2026-01-15 20:51:24 -0600

    agregue el valor password bien a env

--- app/config.php
+++ app/config.php
@@ -11,7 +11,7 @@ $host = $_ENV['DB_HOST'] ;
 $port = $_ENV['DB_PORT'] ;
 $db   = $_ENV['DB_NAME'] ;
 $user = $_ENV['DB_USER'] ;
-$pass = $_ENV['DB_PASSWORD']; 
+$pass = $_ENV['DB_PASS']; 
 
 try {
     $pdo = new PDO(
