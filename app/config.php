b4597dc600043ebba76a162399664a6924d9c30e
GabuasLuis <luis209207@gmail.com>
2026-01-15 21:36:35 -0600

    el config con env

--- app/config.php
+++ app/config.php
@@ -1,21 +1,34 @@
 <?php
-define('SERVIDOR', 'localhost');
-define('USUARIO', 'root');
-define('PASSWORD', '');
-define('BD', 'ventas');
 
-$servidor = "mysql:dbname=".BD.";host=".SERVIDOR;
+// Carga el autoload de Composer (necesario para phpdotenv)
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Carga las variables del .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'] ;
+$port = $_ENV['DB_PORT'] ;
+$db   = $_ENV['DB_NAME'] ;
+$user = $_ENV['DB_USER'] ;
+$pass = $_ENV['DB_PASS']; 
 
 try {
-    $pdo = new PDO($servidor, USUARIO, PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-   //echo "Conexión establecida";
+    $pdo = new PDO(
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+        $user,
+        $pass,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+    // echo "Conectado a Railway";
 } catch (PDOException $e) {
-    //print "Error de conexión: " . $e->getMessage();
-    echo "Error de conexión: " ;
+    die("Error de conexión: " . $e->getMessage());
 }
 
-$URL = "http://localhost/PROYECTO";
+$URL = $_ENV['APP_URL'];
 
 date_default_timezone_set("America/Mexico_City");
 $fechaHora = date("Y-m-d H:i:s");
-
