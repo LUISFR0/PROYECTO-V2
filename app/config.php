5ee517cf09d134e60672ccafa8a2f4d308e8c7e6
GabuasLuis <luis209207@gmail.com>
2026-01-15 20:28:02 -0600

    antes del pull

--- app/config.php
+++ app/config.php
@@ -28,7 +28,7 @@ try {
     die("Error de conexión: " . $e->getMessage());
 }
 
-$URL = "http://localhost/PROYECTO";
+$URL = "http://localhost/PROYECTO-V2";
 
 date_default_timezone_set("America/Mexico_City");
 $fechaHora = date("Y-m-d H:i:s");
